da168c229f441b52ca1c5e3b00f9faff78a8137d
João Bergallo <jhbergallo@gmail.com>
2023-09-21 20:14:46 -0300

    feature: criada a classe custo

---
+++ Custo.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+
+public class Custo {
+    private double valor;
+    private String descricao;
+    private LocalDate data;
+    private String categoria;
+    
+    public Custo(double valor, String descricao, LocalDate data, String categoria){
+        this.valor = valor;
+        this.descricao = descricao;
+        this.data = data;
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
