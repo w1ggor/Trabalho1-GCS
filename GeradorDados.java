01619c8c0c3343d0832926c804c1968dcb68a489
Gabriel Teiga <gabrielteiga99@gmail.com>
2023-09-23 02:10:52 -0300

    feature: metodo geraFuncionarios a partir da quantidade desejada

--- GeradorDados.java
+++ GeradorDados.java
@@ -1,2 +1,22 @@
+import java.util.List;
+
 public class GeradorDados {
+    public void geraFuncionarios(List<Funcionario> lista, int quantidadeFuncionarios, Departamento departamento){
+        String matriculaString;
+        Integer matricula;
+        if (!lista.isEmpty()){
+            matriculaString = lista.get(lista.size()-1).getMatricula();
+            matricula = (Integer.parseInt(matriculaString) + 1);
+
+        } else {
+            matricula = 1;
+        }
+        for (int i = 0; i < quantidadeFuncionarios; i++){
+            matriculaString = matricula.toString();
+            Funcionario funcionario = new Funcionario(matriculaString, "Fulano", departamento);
+            lista.add(funcionario);
+            matricula++;
+        }
+    }
+    
 }
